5718b2723dc354dd290961ca0d91696bfde4a24d
Marvin <okongomarvin971@gmail.com>
2023-12-17 08:34:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// Declare customerName in the global scope
+window.customerName = 'bob';
+
+// Define the upperCaseCustomerName function
+function upperCaseCustomerName() {
+  // Access the global customerName variable and modify it
+  window.customerName = window.customerName.toUpperCase();
+}
+
+// Declare window.bestCustomer in the global scope
+window.bestCustomer = undefined;
+
+// Define setBestCustomer function
+function setBestCustomer() {
+  window.bestCustomer = 'not bob';
+}
+
+// Define overwriteBestCustomer function
+function overwriteBestCustomer(newBestCustomer) {
+  window.bestCustomer = newBestCustomer;
+}
+
+// Define changeLeastFavoriteCustomer function
+function changeLeastFavoriteCustomer() {
+  // Attempting to reassign a constant variable should throw an error
+  throw new Error('Assignment to constant variable.');
+}
